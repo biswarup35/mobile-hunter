69cc1272ecf8678dd0701e1e714e287fe39ca97d
Biswarup35 <biswarup35@gmail.com>
2022-02-12 18:17:50 +0530

    adding pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,4 @@
 export { default as Home } from "./Home";
+export { default as Article } from "./Article";
+export { default as About } from "./About";
+export { default as Privacy } from "./Privacy";
