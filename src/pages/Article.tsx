f1b8ef6a14d9f8ca7407206bff0b14abe5a80cf4
Biswarup35 <biswarup35@gmail.com>
2022-02-12 18:16:50 +0530

    adding article page

---
+++ src/pages/Article.tsx
@@ -0,0 +1,93 @@
+import { Helmet } from "react-helmet";
+import moment from "moment";
+import { Container, Divider, Grid, Stack } from "../components";
+import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+import { Sidebar, Markdown, Loading } from "../views";
+
+const post = (slug: string) => gql`
+  query {
+    articles (where: { slug: "${slug}" }) {
+      title
+      excerpt
+      content
+      updated_at
+      category {
+        name
+      }
+      author {
+        name
+      }
+      cover {
+        url
+        alternativeText
+      }
+    }
+  }`;
+
+const Article = () => {
+  const { slug = "" } = useParams();
+  const { data, loading } = useQuery(post(slug));
+
+  if (loading || !data) return <Loading />;
+  return (
+    <div>
+      <Helmet>
+        <title>{data.articles[0].title}</title>
+        <meta name="description" content={data.articles[0].excerpt} />
+        <meta property="og:type" content="blog" />
+        <meta property="og:title" content={data.articles[0].title} />
+        <meta
+          property="og:image"
+          content={`${process.env.REACT_APP_BLOG_URL}${data.articles[0].cover.url}`}
+        />
+        <meta property="og:description" content={data.articles[0].excerpt} />
+        <meta property="og:site_name" content="Mobile Hunter" />
+        <meta property="og:locale" content="en_IN" />
+        <meta name="twitter:title" content="Content Title" />
+        <meta name="twitter:description" content={data.articles[0].excerpt} />
+        <meta
+          name="twitter:image"
+          content={`${process.env.REACT_APP_BLOG_URL}${data.articles[0].cover.url}`}
+        />
+      </Helmet>
+      <Container className="my-2" maxWidth="lg">
+        <Stack>
+          <h1>{data.articles[0].title}</h1>
+          <p style={{ fontStyle: "italic", color: "gray" }}>
+            {data.articles[0].excerpt}
+          </p>
+          <div className="my-1">
+            <h5>{`By ${data.articles[0].author.name} - ${moment(
+              data.articles[0].updated_at
+            )
+              .startOf("seconds")
+              .fromNow()}`}</h5>
+          </div>
+          <Divider />
+        </Stack>
+      </Container>
+      <Container className="my-2" maxWidth="lg">
+        <Grid container gap={2}>
+          <Grid item xs={12} md={7}>
+            <img
+              className="my-2"
+              style={{ width: "100%", borderRadius: "4px", objectFit: "cover" }}
+              width="600px"
+              height="360px"
+              src={`${process.env.REACT_APP_BLOG_URL}${data.articles[0].cover.url}`}
+              alt={data.articles[0].cover.alternativeText}
+            />
+            <Markdown>{data.articles[0].content}</Markdown>
+            <Divider />
+          </Grid>
+          <Grid item xs={12} md={5}>
+            <Sidebar />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default Article;
