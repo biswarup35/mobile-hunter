b8ca7a043da0f8573d4217bf95bfc91a706de7bf
Biswarup35 <biswarup35@gmail.com>
2022-02-12 18:16:34 +0530

    adding home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,19 @@
+import { Container, Grid } from "../components";
+import { Featured, Sidebar, MainHome } from "../views";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <Container className="my-2">
+      <Featured />
+      <Grid container gap={2}>
+        <Grid item xs={12} md={7} lg={8}>
+          <MainHome />
+        </Grid>
+        <Grid item xs={12} md={5} lg={4}>
+          <Sidebar />
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
