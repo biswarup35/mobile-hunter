7e05d67f71b23be3cfbc79247e3abaf0cf32fddb
Biswarup35 <biswarup35@gmail.com>
2022-02-12 18:20:37 +0530

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home } from "./pages";
+import { Home, Article, About, Privacy } from "./pages";
+import { Navbar, Footer } from "./views";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      {/* <Route path="/about" element={<About />} /> */}
-    </Routes>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path=":slug" element={<Article />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="/privacy" element={<Privacy />} />
+      </Routes>
+      <Footer />
+    </Fragment>
   );
 };
 
